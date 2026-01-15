785068c78cebab76bb94b8c09ea42818bdf17974
Sabari <sabari1007@users.noreply.github.com>
2026-01-15 20:40:18 +0530

    added searchQuery

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error');
 	res.locals.currUser = req.user;
+	res.locals.searchQuery = null;
 	next();
 });
 
